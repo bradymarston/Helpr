3f3525102a1075698e6063c0071f7ecd926bd563
Brady <bradym@outlook.com>
2019-04-25 15:42:47 -0500

    Crested service on client to fetch API data

---
+++ Helpr.Client/Services/ValuesService.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Helpr.Client.Services
+{
+    public class ValuesService
+    {
+        private readonly HttpClient _http;
+
+        public ValuesService(HttpClient http)
+        {
+            _http = http;
+        }
+        public async Task<string[]> GetValuesAsync()
+        {
+            var response = await _http.GetAsync("api/values");
+            var list = JsonConvert.DeserializeObject<string[]>(await response.Content.ReadAsStringAsync());
+            return list;
+        }
+    }
+}
