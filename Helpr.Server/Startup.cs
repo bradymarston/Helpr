5ed92dd861699b64431924f689e5a832c2967f5b
Brady <bradym@outlook.com>
2019-04-30 09:21:22 -0500

    Added ShadySoft.ControllerErrorHelpers

--- Helpr.Server/Startup.cs
+++ Helpr.Server/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ShadySoft.ControllerErrorHelpers.Extensions.DepedencyInjection;
 using ShadySoft.EntityPersistence;
 
 namespace Helpr.Server
@@ -51,6 +52,8 @@ namespace Helpr.Server
 
             services.AddControllers()
                 .AddNewtonsoftJson();
+
+            services.AddControllerErrorHelpers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
